ce3d7373c34c6d006acea4fa43b4de595b01a571
Amadeus <a.turner3x@gmail.com>
2023-06-05 14:38:59 -0400

    updated .gitignore

---
+++ test-connection.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Create a new Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
+
+// Test the connection
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+// Call the testConnection function
+testConnection();
