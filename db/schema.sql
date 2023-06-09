0181e6db8b851bf5a6c033638b941dd7f3b0c365
Amadeus <a.turner3x@gmail.com>
2023-06-09 04:22:43 -0400

    changed schema.sql and product-seeds syntax

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,32 @@ CREATE DATABASE ecommerce_db;
 
 -- USE DATABASE
 USE ecommerce_db;
+
+
+-- Create Category table
+CREATE TABLE category (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255)
+);
+
+-- Create Product table
+CREATE TABLE product (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL
+);
+
+-- Create Tag table
+CREATE TABLE tag (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+-- Create ProductTag table
+CREATE TABLE product_tag (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  product_id INTEGER REFERENCES product(id),
+  tag_id INTEGER REFERENCES tag(id)
+);
+
+
